fd54058f9f2cfde1e26f5332598c6ef901e92529
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-03-30 15:28:06 +0000

    implement picnics global listing with both json and html

--- src/routes/tandem.js
+++ src/routes/tandem.js
@@ -2,9 +2,22 @@ var express = require('express')
 const Tandem = require('../tandem')
 var router = express.Router()
 
-/* GET users listing. */
+/* GET tandems listing. */
 router.get('/', function (req, res, next) {
-  res.send(Tandem.list)
+  if(req.query.view === 'json') {
+    return res.send(
+      Tandem.list.map(tandem => ({
+        date: tandem.date,
+        time: tandem.time,
+        language: tandem.language,
+        status: tandem.status,
+        partner: tandem.partner.name,
+      }))
+    )
+  }
+  res.render('tandems', {
+    tandems: Tandem.list,
+  })
 })
 
 module.exports = router
