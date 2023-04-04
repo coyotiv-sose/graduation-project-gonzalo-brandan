e5279a811ac400be6540715dea3937175ff12756
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-04-04 11:40:57 +0000

    conect successfully to mongodb

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log('hello world')
-console.log('i am gonzo')
-console.log('and this is js301')
-
-setTimeout(() => {
-  console.log('i am a timeout')
-}, 2000)
-
-console.log('I hope is that easy')
