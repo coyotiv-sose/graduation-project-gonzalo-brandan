9dac0538ce9b849e26108fd37b2edab46df62980
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-04-05 18:46:25 +0000

    get user list rendering json

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,7 +5,8 @@ const User = require('../models/user')
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
   const users = await User.find()
-  res.send(users)
+  if (req.query.view === 'json') return res.send(users)
+  res.render('users', { users })
 })
 
 // get a specific user by index
@@ -15,9 +16,10 @@ router.get('/:userId', function (req, res, next) {
 
 // create a new user
 router.post('/', function (req, res, next) {
-  //const user = new User(req.body.name)
   const user = User.create({ name: req.body.name })
   res.send(user)
 })
+
+// delete a user
 
 module.exports = router
