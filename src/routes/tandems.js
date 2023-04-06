fd859eac9ffc6ebfac2bc4b6e1114e76d7841469
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-04-06 17:15:36 +0000

    remove unnecesary code

--- src/routes/tandems.js
+++ src/routes/tandems.js
@@ -19,26 +19,9 @@ router.get('/:tandemId', function (req, res, next) {
 // initiate a new tandem
 router.post('/', async function (req, res, next) {
   const user = await User.findById({ _id: req.body.user })
-  console.log(user)
   const partner = await User.findById({ _id: req.body.partner })
   const tandem = await user.initiateTandem(partner, req.body.language, req.body.date, req.body.time)
-
   res.send(tandem.details)
-  // res.send({
-  //   name: user.name,
-  //   targetLanguage: user.targetLanguage,
-  //   offeredLanguage: user.offeredLanguage,
-  //   tandems: user.tandems.map(tandem => {
-  //     return {
-  //       user: tandem.user.name,
-  //       //partner: tandem.partner.name,
-  //       language: tandem.language,
-  //       date: tandem.date,
-  //       time: tandem.time,
-  //       status: tandem.status,
-  //     }
-  //   }),
-  // })
 })
 
 module.exports = router
