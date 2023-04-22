1d7a210ae8237340039f8fb7d2d9b88daa9ac104
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-04-22 10:19:05 +0000

    add express-session

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,35 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 require('dotenv').config()
 require('./database-connection')
 const cors = require('cors')
+const session = require('express-session')
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
-var tandemsRouter = require('./routes/tandems') // import the tandemRouter
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const tandemsRouter = require('./routes/tandems') // import the tandemRouter
 
-var app = express()
+const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+
+app.use(
+  session({
+    secret: 'jsda3231jsakl1233kk',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 1000 * 60 * 60 * 24 * 15, // 15 days
+    },
+  })
+)
+
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
