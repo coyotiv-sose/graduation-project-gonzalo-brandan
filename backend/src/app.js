7d3dcc39ef71b0166a074cf632ee9466f6f10d79
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-04-22 11:17:43 +0000

    add cconnect mongo, express-connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,9 @@ require('dotenv').config()
 require('./database-connection')
 const cors = require('cors')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
+
+const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -18,6 +21,8 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+const connectionPromise = mongoose.connection.asPromise().then(connection => (connection = connection.getClient()))
+
 app.use(
   session({
     secret: 'jsda3231jsakl1233kk',
@@ -27,8 +32,24 @@ app.use(
       secure: process.env.NODE_ENV === 'production',
       maxAge: 1000 * 60 * 60 * 24 * 15, // 15 days
     },
+    store: MongoStore.create({
+      //mongoUrl: process.env.MONGODB_CONNECTION_STRING,
+      clientPromise: connectionPromise,
+    }),
   })
 )
+
+app.use((req, res, next) => {
+  const numberOfVisits = req.session.numberOfVisits || 0
+  req.session.numberOfVisits = numberOfVisits + 1
+  req.session.history = req.session.history || []
+  req.session.history.push({ url: req.url, ip: req.ip })
+  req.session.ip = req.ip
+
+  console.log('session', req.session)
+
+  next()
+})
 
 app.use(cors())
 app.use(logger('dev'))
