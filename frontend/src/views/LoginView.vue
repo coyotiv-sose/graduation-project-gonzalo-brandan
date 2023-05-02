c070ac57b1c5633b646df9b0f8af71726dac16b8
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-05-02 14:46:10 +0000

    use bootstrap for email, password and login button

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -19,51 +19,33 @@ export default {
       this.$router.push('/')
     }
   }
-
-  // methods: {
-  //   async login() {
-  //     const request = await axios.post(
-  //       'http://localhost:3000/accounts/session',
-  //       {
-  //         email: this.email,
-  //         password: this.password
-  //       },
-  //       {
-  //         withCredentials: true
-  //       }
-  //     )
-  //     console.log(request.data)
-  //     this.user = request.data
-  //   }
-  // }
 }
 </script>
 
 <template lang="pug">
-h2 Login to Lingolink
-form(@submit.prevent="doLogin")
-  div
-    label(for="email") Email:
-    input#email(v-model="email" type="text" required)
+.container
+  h2 Login to Lingolink
+  form(@submit.prevent="doLogin")
+    div#emailInput
+      label.form-label(for="email") Email:
+      input.form-control#email(v-model="email" type="text" required)
 
-  div
-    label(for="password") Password:
-    input#password(v-model="password" type="password" required)
+    div.mb-3
+      label.form-label(for="password") Password:
+      input.form-control#password(v-model="password" type="password" required)
+    div.loginButton
+      button.btn.btn-primary(type="submit") Log in
 
-  button(type="submit") Log in
-//- p(v-if="user") You re log in as {{ user.name }}
-//- form(@submit.prevent="login")
-//-   div
-//-     label(for="email") Email
-//-     input#email(type="email" v-model="email" required)
-
-//-   div
-//-     label(for="password") Password
-//-     input#password(type="password" v-model="password" required)
-
-//-   button(type="submit") Login
-
-//-   p
-//-     | Don't have an account?
-//-     router-link(to="/signup") Sign up
 </template>
+<style scoped>
+#emailInput {
+  margin-bottom: 1rem;
+}
+.loginButton {
+  margin: auto;
+}
+.container {
+  max-width: 400px;
+  margin-top: 2rem;
+}
+</style>
