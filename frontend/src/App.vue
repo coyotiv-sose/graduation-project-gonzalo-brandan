662c36312a75ea18a7e6773ce0508381f921db55
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-05-04 15:33:27 +0000

    add a navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,10 +4,12 @@ import HelloWorld from './components/HelloWorld.vue'
 import { useAccountStore } from './stores/account'
 import { mapActions, mapState } from 'pinia'
 //import axios from 'axios'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
+    NavBar,
     HelloWorld,
     RouterLink,
     RouterView
@@ -25,83 +27,9 @@ export default {
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/tandems">Tandems</RouterLink>
-        <RouterLink v-if="!user" to="/login">Log in</RouterLink>
-        <RouterLink v-if="!user" to="/signup">Sign up</RouterLink>
-        <a v-if="user" @click="logout">Log out</a>
-      </nav>
-    </div>
-  </header>
+  <NavBar />
   <!--<h1>Lingolink for {{ user?.name }}</h1>-->
   <Suspense>
     <RouterView />
   </Suspense>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<script></script>
+<template lang="pug">
+nav.navbar.navbar-expand-lg.bg-body-tertiary
+  .container-fluid
+    a.navbar-brand(href='#') Navbar
+    button.navbar-toggler(type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation')
+      span.navbar-toggler-icon
+    #navbarSupportedContent.collapse.navbar-collapse
+      ul.navbar-nav.me-auto.mb-2.mb-lg-0
+        li.nav-item
+          a.nav-link.active(aria-current='page' href='#') Home
+        li.nav-item
+          router-link.nav-link(to="/tandems") Tandems
+        li.nav-item(v-if="!user")
+          router-link.nav-link(to="/login") Log in
+        li.nav-item(v-if="!user")
+          router-link.nav-link(to="/signup") Sign up
+        li.nav-item(v-if="user")
+          a.nav-link(@click="logout") Log out
+
+</template>
