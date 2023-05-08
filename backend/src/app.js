739338ef2bf9df0c790fe89d00c8203124505366
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-05-08 08:58:28 +0000

    add sameSite: 'none'

--- backend/src/app.js
+++ backend/src/app.js
@@ -43,6 +43,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === 'production',
       maxAge: 1000 * 60 * 60 * 24 * 15, // 15 days
+      sameSite: 'none', //make it dependent if production, otherwise sameSite: 'lax'
     },
     store: MongoStore.create({
       //clientPromise: clientPromise,
