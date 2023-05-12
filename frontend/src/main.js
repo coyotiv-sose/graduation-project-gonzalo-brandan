051bbec71df8ebacd716cf7c332e8621636c9e24
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-05-12 13:10:58 +0000

    remove comments

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,15 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-
-//import { useCounterStore } from './stores/counter' //
-
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-//app.component('Counter', Counter) //
 app.use(router)
 
 app.mount('#app')
