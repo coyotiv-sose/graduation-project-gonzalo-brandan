59e16f4644d0d2bd33fb31e278d1c1a7d322ac7a
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-05-16 15:29:23 +0000

    correct NODE to NODE_ENV

--- backend/src/app.js
+++ backend/src/app.js
@@ -43,7 +43,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === 'production',
       maxAge: 1000 * 60 * 60 * 24 * 15, // 15 days
-      sameSite: process.env.NODE === 'production' ? 'none' : 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       //sameSite: 'lax', //make it dependent if production, otherwise sameSite: 'lax'
       // domain: process.env.NODE_ENV === 'production' ? 'https://backend-jdi5rgnuxa-ew.a.run.app' : 'localhost:3000', //heroku replace by backend url google
       domain: process.env.COOKIE_DOMAIN,
