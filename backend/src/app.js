9bf8765c668c16f4a3e8e4a2123717a18e012e40
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-05-17 07:34:12 +0000

    delete traces of index.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,7 +21,6 @@ passport.use(User.createStrategy())
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const tandemsRouter = require('./routes/tandems')
 const accountsRouter = require('./routes/accounts')
@@ -83,7 +82,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter) //
+//app.use('/', indexRouter) //
 app.use('/tandems', tandemsRouter) // when the path is /tandem, use the tandemRouter
 app.use('/users', usersRouter) // when the path is /users, use the usersRouter
 
