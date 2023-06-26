6312ea4ef35179b44b7d62a58804b23378d7d821
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-06-26 16:58:40 +0000

    add navbar link and try for free

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -15,7 +15,8 @@ export default {
 <template lang="pug">
 nav.navbar.navbar-expand-lg.bg-body-tertiary
   .container-fluid
-    a.navbar-brand(href='#') Lingolink
+    a.navbar-brand(href='/')
+      img(src="../assets/brand/logo.svg" alt="brand")
     button.navbar-toggler(type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation')
       span.navbar-toggler-icon
     #navbarSupportedContent.collapse.navbar-collapse
@@ -25,9 +26,11 @@ nav.navbar.navbar-expand-lg.bg-body-tertiary
         li.nav-item
           router-link.nav-link(to="/users") Users
         li.nav-item(v-if="!user")
-          router-link.nav-link(to="/login") Log in
+          router-link.nav-link.me-5(to="/login") Log in
+        //li.nav-item(v-if="!user")
+        //  router-link.nav-link(to="/signup") Sign up
         li.nav-item(v-if="!user")
-          router-link.nav-link(to="/signup") Sign up
+          a.btn.btn-outline-secondary.shadow-sm.-d-block(href="/signup") Try for free
         li.nav-item(v-if="user")
           router-link.nav-link(to="/myavailabilities") My Availabilities
         li.nav-item.dropdown(v-if="user")
