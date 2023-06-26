5696b2d8c4c093c221c0e2fcd9c8d2e9a4a56143
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-06-26 17:18:24 +0000

    add lingolink logo

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -16,7 +16,7 @@ export default {
 nav.navbar.navbar-expand-lg.bg-body-tertiary
   .container-fluid
     a.navbar-brand(href='/')
-      img(src="../assets/brand/logo.svg" alt="brand")
+      img(src="../assets/brand/logo-lingolink.png" alt="brand")
     button.navbar-toggler(type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation')
       span.navbar-toggler-icon
     #navbarSupportedContent.collapse.navbar-collapse
